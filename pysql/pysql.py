384ef6a66b19087637e590b897b3b17cff5d971b
KosukeAbe1020 <87123183+KosukeAbe1020@users.noreply.github.com>
2023-12-29 13:20:34 +0900

    Update pysql.py

--- pysql/pysql.py
+++ pysql/pysql.py
@@ -47,7 +47,7 @@ class Data_Source(BaseModel):
 
     def find(self,txt:str,EXACT_MATCH=0):
         if EXACT_MATCH == 0:
-            return df[self.data_source.apply(lambda row: any(txt.upper() in str(cell).upper() for cell in row), axis=1)]
+            return self.data_source[self.data_source.apply(lambda row: any(txt.upper() in str(cell).upper() for cell in row), axis=1)]
         elif EXACT_MATCH == 1:
             return self.data_source[self.data_source.apply(lambda row: any(txt in str(cell) for cell in row), axis=1)]
         else:
